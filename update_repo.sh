b2e5c05b86357795abc8e7e50c0f084340981d83
jcairel <jcaireljr@comcast.net>
2024-10-22 00:03:09 -0700

    fix scripts

--- update_repo.sh
+++ update_repo.sh
@@ -16,19 +16,19 @@ if [ $? != 0 ]; then
     exit 1
 fi
 # get id
-COMMIT_ID='echo $COMMIT | awk '{ print $2 }''
+COMMIT_ID=`echo $COMMIT | awk '{ print $2 }'`
 
 # get most recent commit again
-run_or_fail "Could not pull from repository" git pull
+run_or_fail "Could not pull from repository" git pull origin main
 COMMIT=$(run_or_fail "Could not call 'git log' on repository" git log -n1)
 if [ $? != 0 ]; then
     echo "Could not call 'git log' on repository"
     exit 1
 fi
-NEW_COMMIT_ID='echo $COMMIT | awk '{ print $2 }''
+NEW_COMMIT_ID=`echo $COMMIT | awk '{ print $2 }'`
 
 # if id changed, write it to a file
-if [ $NEW_COMMIT_ID != $COMMIT_ID ]; then
+if [ "$NEW_COMMIT_ID" != "$COMMIT_ID" ]; then
     popd 1> /dev/null
-    echo $NEW_COMMIT_ID > .commit_id
+    echo "$NEW_COMMIT_ID" > .commit_id
 fi
